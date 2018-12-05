585d536039f3ffba43aa68caf0fd61ebdeacbc1e
DaMoo <git@damoo.zone>
2018-12-05 18:58:06 -0500

    Complete day 1 - part 1

---
+++ day1/csharp/day1/Day1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\peter\Developer\advent-of-code-2018\day1\input.txt");
+            Console.WriteLine($"Part 1: {Part1(lines)}");
+            Console.ReadKey();
+        }
+
+        static int Part1(string[] input)
+        {
+            return input.Select(s => int.Parse(s)).Sum();
+        }
+    }
+}
