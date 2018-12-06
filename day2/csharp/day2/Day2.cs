17f8eba609855ad0ea120848159a9febe89fe742
DaMoo <git@damoo.zone>
2018-12-06 15:31:09 -0500

    Complete day 2 - part 2

--- day2/csharp/day2/Day2.cs
+++ day2/csharp/day2/Day2.cs
@@ -5,32 +5,58 @@ using System.Linq;
 
 namespace day2
 {
-    class Day2
+    internal class Day2
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines(@"C:\Users\peter\Developer\advent-of-code-2018\day2\input.txt");
-            Console.WriteLine($"Part 1: {Part1(lines)}");
+            var lines = File.ReadAllLines(@"C:\Users\peter\Developer\advent-of-code-2018\day2\input.txt");
+            Console.WriteLine($"Part 1: {Part1(lines)}\nPart 2: {Part2(lines)}");
             Console.ReadKey();
         }
 
-        static int Part1(string[] input)
+        private static int Part1(IEnumerable<string> input)
         {
-            int doubleLetterCount = 0;
-            int tripleLetterCount = 0;
+            var doubleLetterCount = 0;
+            var tripleLetterCount = 0;
 
-            foreach(string boxId in input)
+            foreach (var boxId in input)
             {
-                Dictionary<char, int> seenDict = new Dictionary<char, int>();
-                foreach(char character in boxId)
-                {
-                    seenDict[character] = seenDict.GetValueOrDefault(character) + 1;
-                }
+                var seenDict = new Dictionary<char, int>();
+                foreach (var character in boxId) seenDict[character] = seenDict.GetValueOrDefault(character) + 1;
                 if (seenDict.Values.Contains(2)) doubleLetterCount++;
                 if (seenDict.Values.Contains(3)) tripleLetterCount++;
             }
 
             return doubleLetterCount * tripleLetterCount;
+        }
+
+        private static string Part2(IEnumerable<string> input)
+        {
+            var matchingBoxIDs = input
+                .SelectMany((boxId1, i) => input
+                    .Where((boxId2, j) => i > j)
+                    .Select(boxId2 => Tuple.Create(boxId1, boxId2)))
+                .Single(pair => HammingDistance(pair.Item1, pair.Item2) == 1);
+
+            var indexOfMismatch = -1;
+
+            for (var i = 0; i < matchingBoxIDs.Item1.Length; i++)
+            {
+                if (matchingBoxIDs.Item1[i] == matchingBoxIDs.Item2[i]) continue;
+                indexOfMismatch = i;
+                break;
+            }
+
+            var commonChars = string.Concat(matchingBoxIDs.Item1.Substring(0, indexOfMismatch),
+                matchingBoxIDs.Item1.Substring(indexOfMismatch + 1));
+            return commonChars;
+        }
+
+        private static int HammingDistance(string first, string second)
+        {
+            return first
+                .Zip(second, (firstChar, secondChar) => new {firstChar, secondChar})
+                .Count(tuple => tuple.firstChar != tuple.secondChar);
         }
     }
 }
