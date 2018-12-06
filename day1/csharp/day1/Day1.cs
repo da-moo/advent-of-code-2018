f044f131fae552b89e07549c11e8670e907f7070
DaMoo <git@damoo.zone>
2018-12-05 19:56:53 -0500

    Implement day 1 - part 2

--- day1/csharp/day1/Day1.cs
+++ day1/csharp/day1/Day1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,14 +9,34 @@ namespace day1
     {
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines(@"C:\Users\peter\Developer\advent-of-code-2018\day1\input.txt");
-            Console.WriteLine($"Part 1: {Part1(lines)}");
+            string[] lines = File.ReadAllLines(@"input.txt");
+            Console.WriteLine($"Part 1: {Part1(lines)}\nPart 2: {Part2(lines)}");
             Console.ReadKey();
         }
 
         static int Part1(string[] input)
         {
-            return input.Select(s => int.Parse(s)).Sum();
+            return input.Select(int.Parse).Sum();
+        }
+
+        static int Part2(string[] input)
+        {
+            var frequencyChanges = input.Select(int.Parse);
+            HashSet<int> seenFrequencyChanges = new HashSet<int>();
+            int cumulative = 0;
+
+            do
+            {
+                foreach (int frequencyChange in frequencyChanges)
+                {
+                    cumulative += frequencyChange;
+                    if (seenFrequencyChanges.Contains(cumulative))
+                    {
+                        return cumulative;
+                    }
+                    seenFrequencyChanges.Add(cumulative);
+                }
+            } while (true);
         }
     }
 }
